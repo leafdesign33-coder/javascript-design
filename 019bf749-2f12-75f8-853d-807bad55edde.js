0833101b25d30f76f960c8b81df8c60e37fe1396
leafdesign33-coder <leafdesign25@gmail.com>
2026-01-27 11:39:47 +0100

    Fix syntax error in menu array structure

--- 019bf749-2f12-75f8-853d-807bad55edde.js
+++ 019bf749-2f12-75f8-853d-807bad55edde.js
@@ -9,7 +9,8 @@
     {
       name: "menu",
       navIcon: "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M4 6h16M4 12h16M4 18h16' stroke='black' stroke-width='2' stroke-linecap='round'/></svg>"
-    }]
+    }
+    ]
   ];
 
   const container = document.getElementById('root');
