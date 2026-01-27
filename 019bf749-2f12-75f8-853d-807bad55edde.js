95b3f72a90778c519a379915c39c36bdc5c86c1c
leafdesign33-coder <leafdesign25@gmail.com>
2026-01-27 11:25:56 +0100

    Update 019bf749-2f12-75f8-853d-807bad55edde.js

--- 019bf749-2f12-75f8-853d-807bad55edde.js
+++ 019bf749-2f12-75f8-853d-807bad55edde.js
@@ -4,11 +4,12 @@
   // Array mit Objekten, die den SVG-Code als navIcon enthalten
   const rootArray = [
     { name: "javascript-design" },
-    { name: "", navIcon: "" },
+    nav:[
+    
     {
       name: "menu",
       navIcon: "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M4 6h16M4 12h16M4 18h16' stroke='black' stroke-width='2' stroke-linecap='round'/></svg>"
-    }
+    }]
   ];
 
   const container = document.getElementById('root');
